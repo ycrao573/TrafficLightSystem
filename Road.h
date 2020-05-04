2a6df96f90149c66343ae12d9eaaaed09dd2f7e8
LoganClark010 <63857085+LoganClark010@users.noreply.github.com>
2020-05-04 15:40:09 +0100

    Addition of Internal Commentary

--- Road.h
+++ Road.h
@@ -6,26 +6,29 @@
 #define TRAFFICLIGHTSYSTEM_ROAD_H
 
 #include "TrafficLight.h"
+//required for random generator function
 #include "stdlib.h"
 
+//class to store attributes and methods associated with a road
 class Road {
 public:
+    //constructor - creates instance when no data is provided
     Road();
+    // constructor - creates instance when data is provided
     Road(char, int, int);
-    //~Road();
     char getDirection() const;
     int getLength() const;
     int getWidth() const;
+    //variables associated to start point of road
     int str_x, str_y;
     void setStrPt(int, int);
-    int delayPtr[4] = {1,1,5,1};
-    TrafficLight *trafficLight = new TrafficLight(delayPtr);
+    //each road contains one traffic light
+    //implemented via creation of instance pointed to via a pointer
+    TrafficLight *trafficLight = new TrafficLight();
     string roadName(char);
 private:
     char direction;
     int length, width;
-
 };
-
 
 #endif //TRAFFICLIGHTSYSTEM_ROAD_H
