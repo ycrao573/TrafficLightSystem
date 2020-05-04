ec7e2bd33d1eb7a51149816f173015f20ca00cfe
LoganClark010 <63857085+LoganClark010@users.noreply.github.com>
2020-05-04 16:15:26 +0100

    Addition of Internal Commentary

--- User.cpp
+++ User.cpp
@@ -1,14 +1,16 @@
 #include "User.h"
+//required for inputtig and outputting data
 #include <iostream>
 
 using namespace std;
 
+//function definition for constructor - no data provided
 User::User() {
+    //initialises start point to predefined position
     setPt(0, 0);
 }
 
-//operator+ function definition for when...
-//increase numOfCars button is selected
+//function definiton for adding more cars to the road
 User User::operator+(User a) {
     //create new instance of User class
     User c;
@@ -17,8 +19,7 @@ User User::operator+(User a) {
     return c;
 }
 
-//operator- function definition for when...
-//decrease numOfCars button is selected
+//function definiton for adding more cars to the road
 User User::operator-(User a) {
     //create new instance of User class
     User c;
@@ -27,31 +28,39 @@ User User::operator-(User a) {
     return c;
 }
 
+//function definiton for stopping user movement
 void User::stop() {
     setVelocity(0);
 }
 
+//function definiton for starting user movement
 void User::start() {
+    //the velocity is set to a constant speed
     setVelocity(speed);
 }
 
+//function definiton for setting the user's velocity
 void User::setVelocity(int velocity) {
     this->velocity = velocity;
 }
 
+//function definiton for setting user's position on the map
 void User::setPt(int x, int y) {
     this->x = x;
     this->y = y;
 }
 
+//function definiton for returning user's speed
 int User::getSpeed() {
     return speed;
 }
 
+//function definiton for setting user's speed
 void User::setSpeed(int speed) {
     this->speed = speed;
 }
 
+//function definiton for returning user's velocity
 int User::getVelocity() {
     return velocity;
 }
