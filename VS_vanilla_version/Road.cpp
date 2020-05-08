59ae5683e2a0591720e5caa3c5dfc739503d189e
Yuchen Rao <44971335+ycrao573@users.noreply.github.com>
2020-05-08 22:35:37 +0800

    add comment

--- VS_vanilla_version/Road.cpp
+++ VS_vanilla_version/Road.cpp
@@ -20,6 +20,7 @@ Road::Road(char direction, int length, int width) {
     this->width = width;
 }
 
+//function defintion for a destructor for road class
 Road::~Road() {
     if(trafficLight!=nullptr)
         delete trafficLight;
